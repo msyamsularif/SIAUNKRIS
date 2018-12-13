0864020ed36deab762339cd3f26fec2693b72139
willy adinata saragih <willy.adinata@gmail.com>
2018-12-14 03:37:48 +0700

    Penambahan DB Prodi, Dosen, Mahasiwa

--- siaunkris.sql
+++ siaunkris.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Des 2018 pada 14.26
+-- Waktu pembuatan: 13 Des 2018 pada 21.36
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 5.6.38
 
@@ -21,6 +21,77 @@ SET time_zone = "+00:00";
 --
 -- Database: `siaunkris`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_dosen`
+--
+
+CREATE TABLE `data_dosen` (
+  `nip` int(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_telp` int(50) NOT NULL,
+  `prodi` varchar(100) NOT NULL,
+  `matkul` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_dosen`
+--
+
+INSERT INTO `data_dosen` (`nip`, `nama`, `email`, `no_telp`, `prodi`, `matkul`, `status`) VALUES
+(275875664, 'Sumanto Prayogi', 'example@email.com', 812555788, 'Informatika', 'Pemrograman Web', 'Aktif'),
+(275875665, 'Jono Sumito', 'example@email.com', 81265654, 'Informatika', 'Algoritma', 'Aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_mahasiswa`
+--
+
+CREATE TABLE `data_mahasiswa` (
+  `nim` int(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_telp` int(50) NOT NULL,
+  `prodi` varchar(100) NOT NULL,
+  `shift` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_mahasiswa`
+--
+
+INSERT INTO `data_mahasiswa` (`nim`, `nama`, `email`, `no_telp`, `prodi`, `shift`, `status`) VALUES
+(1770231050, 'Willy Adinata Saragih', 'willy.adinata@gmail.com', 628165744, 'Informatika', 'Reguler', 'Aktif'),
+(1770231051, 'Syamsul', 'example@email.com', 76665547, 'Informatika', 'Reguler', 'Aktif'),
+(1770231052, 'Rommy Andiansyah', 'example@email.com', 667546545, 'Informatika', 'Reguler', 'Aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_prodi`
+--
+
+CREATE TABLE `data_prodi` (
+  `kode` varchar(100) NOT NULL,
+  `no_ijin` varchar(100) NOT NULL,
+  `nama_prodi` varchar(100) NOT NULL,
+  `ketua_prodi` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_prodi`
+--
+
+INSERT INTO `data_prodi` (`kode`, `no_ijin`, `nama_prodi`, `ketua_prodi`, `email`, `status`) VALUES
+('KP-TIF', 'NO.35/27/007/2003', 'Informatika', 'Surmanto Sitio', 'informatika@unkris.ac.id', 'Aktif');
 
 -- --------------------------------------------------------
 
@@ -49,6 +120,24 @@ INSERT INTO `users` (`id`, `nama`, `username`, `password`, `level`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `data_dosen`
+--
+ALTER TABLE `data_dosen`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- Indeks untuk tabel `data_mahasiswa`
+--
+ALTER TABLE `data_mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indeks untuk tabel `data_prodi`
+--
+ALTER TABLE `data_prodi`
+  ADD PRIMARY KEY (`kode`);
 
 --
 -- Indeks untuk tabel `users`
